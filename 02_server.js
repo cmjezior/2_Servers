e69ac432523e2e76e5f48e8b49a39ae92b796499
Chris Jezior <chrisjezior@Chriss-MacBook-Pro.local>
2018-02-17 11:56:53 -0500

    updated syntax and code structure

--- 02_server.js
+++ 02_server.js
@@ -2,42 +2,32 @@
 var http = require("http");
 
 // Define a port to listen for incoming requests
-var PORT1 = 7000;
-var PORT2 = 7500
+var PORTONE = 7000;
+var PORTTWO = 7500
 
 // Create a generic function to handle requests and responses
-function handleRequest1(request, response) {
-
-  // Send the below string to the client when the user visits the PORT URL
+// Send the below string to the client when the user visits the PORT URL
+function handleRequestOne(request, response) {
   response.end("You DON'T have bad breathe in the morning." + request.url);
 }
 
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest1);
-
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT1, function() {
-
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT1);
-});
-
-// ------
-
-function handleRequest2(request, response) {
-
-  // Send the below string to the client when the user visits the PORT URL
+function handleRequestTwo(request, response) {
   response.end("Your pores are huge and your nail beds suck." + request.url);
 }
 
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest2);
+var serverOne = http.createServer(handleRequestOne);
+var serverTwo = http.createServer(handleRequestTwo);
 
 // Start our server so that it can begin listening to client requests.
-server.listen(PORT2, function() {
+// Log (server-side) when our server has started
+server.listen(PORTONE, function() {
 
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT2);
+  console.log("Server listening on: http://localhost:" + PORTONE);
+});
+
+server.listen(PORTWO, function() {
+
+  console.log("Server listening on: http://localhost:" + PORTTWO);
 });
